1aef9ccd6ec4322a692ccf3c07ed314941e25b22
atockar <atockar@users.noreply.github.com>
2014-04-29 12:08:53 -0500

    Update BayesNetwork.py

--- BayesNetwork.py
+++ BayesNetwork.py
@@ -26,6 +26,7 @@ class BayesNetwork:
         def __init__(self):
             self.prob = []
             self.evidence = []
+            self.belief = []
 
         #  Option to add values and possibly distribution probabilities separately
         def setProbDist(self, probabilities = []):
@@ -36,13 +37,16 @@ class BayesNetwork:
             print('Name:\t' + self.name)
             print('Values:\t' + str(self.values))           # Maybe make this a bit prettier
             print('Distribution:\t' + str(self.prob))
-            if self.evidence != self.prob:
+            if self.evidence:
                 print('Evidence:\t' + str(self.evidence))       # Only show if evidence exists
+            if self.belief
+                print('Belief:\t' + str(self.belief))       # Only show if belief exists
             print('Parents:\t' + str(self.parents))         # Change to for loop with names
 
         # Clear all evidence
         def clearEvidence(self):
             self.evidence = []
+            print('Evidence cleared')
 
         # Set evidence
         def setEvidence(self, value):
@@ -50,6 +54,15 @@ class BayesNetwork:
                 if self.values[i] == value:
                     self.evidence.append(1)
                 else: self.evidence.append(0)
+
+        # Get belief
+        def getBelief(self, value):
+            if self.belief:
+                print('P(Some|shit) = ' + str(self.belief))
+                return self.belief
+            else:
+                print('P(Some other shit) = ' + str(self.prob))
+                return self.prob
 
     # Method to add nodes
     def addNode(self, name, values, parents = []):
@@ -73,6 +86,18 @@ class BayesNetwork:
         for node in self.nodes:
             print(node.name)
 
+    # Calculate belief - would be good if this can be executed along with setEvidence
+    def calcBelief(self):
+        # Calculate all beliefs in network given the evidence at hand
+
+        # Find all nodes with evidence
+        evidenceNodes = []
+        for node in self.nodes:
+            if node.evidence:
+                evidenceNodes.append(node)
+
+        # Do some hardcore formula to pull out the probabilities and evidence and infer the belief
+
 ### Test functionality
 
 bn = BayesNetwork('BN1')
@@ -89,14 +114,14 @@ smokerNode.setProbDist([
 [[0.5, 0.2, 0.3],[0.5,0.25,0.25]],
 ])
 
-#cancerNode = bn.addNode('Cancer', ['None','Benign','Malignant'])
-#cancerNode.setProbDist([[0.96,0.88,0.6],[0.03,0.08,0.25],[0.01,0.04,0.15]])
-#cancerdistribution = [0.935, 0.046, 0.019]
+# cancerNode = bn.addNode('Cancer', ['None','Benign','Malignant'])
+# cancerNode.setProbDist([[0.96,0.88,0.6],[0.03,0.08,0.25],[0.01,0.04,0.15]])
+# cancerdistribution = [0.935, 0.046, 0.019]
 
 ageNode.observe()
 genderNode.observe()
 smokerNode.observe()
-#cancerNode.observe()
+# cancerNode.observe()
 
 tempNode = bn.addNode('Temp', ['A','B','C'])
 bn.removeNode(tempNode)
@@ -107,3 +132,4 @@ ageNode.clearEvidence()
 ageNode.observe()
 ageNode.setEvidence('old')
 ageNode.observe()
+calcBelief()
